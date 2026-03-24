ff3c89adb55755d8b2b66e659c287af4c7c37562
ankur kumar singh <ankur1408@gmail.com>
2026-03-24 12:40:16 +0530

    Fix Jenkins Docker agent and Django static files configuration

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -119,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'todo' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Allow all hosts for development
+ALLOWED_HOSTS = ['*']
